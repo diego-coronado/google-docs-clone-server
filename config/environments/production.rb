eb2f8ababe14fd9d594b0ef559564d52961a003e
Diego Coronado <dcoronado@able.co>
2021-07-10 21:12:45 -0500

    fix allowed origins

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -43,8 +43,7 @@ Rails.application.configure do
   # Mount Action Cable outside main process or domain.
   # config.action_cable.mount_path = nil
   config.action_cable.url = 'wss://peaceful-beach-58495.herokuapp.com/cable'
-  config.action_cable.allowed_request_origins = ['http://peaceful-beach-58495.herokuapp.com',
-                                                 %r{http://peaceful-beach-58495.herokuapp.com.*}]
+  config.action_cable.allowed_request_origins = ['https://shielded-ocean-24840.herokuapp.com']
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
