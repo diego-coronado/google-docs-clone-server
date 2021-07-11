4208a524cf8362bd16b3d5886f30f37cd278702c
Diego Coronado <dcoronado@able.co>
2021-07-10 22:14:04 -0500

    test cors config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -43,7 +43,7 @@ Rails.application.configure do
   # Mount Action Cable outside main process or domain.
   # config.action_cable.mount_path = nil
   config.action_cable.url = 'wss://peaceful-beach-58495.herokuapp.com/cable'
-  config.action_cable.allowed_request_origins = ['https://shielded-ocean-24840.herokuapp.com']
+  config.action_cable.allowed_request_origins = ['*']
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
